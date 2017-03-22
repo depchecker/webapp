fc3880bf00ad3e5cad7ca976124e528e55ff2ea0
Simone Vittori <hello@simonewebdesign.it>
2017-03-22 19:38:54 +0000

    Put a test in place

--- test/depchecker_test.exs
+++ test/depchecker_test.exs
@@ -1,4 +1,23 @@
 defmodule DepCheckerTest do
   use ExUnit.Case
   doctest DepChecker
+
+  test "it works" do
+    expectation =
+      %{"phoenix" => %{
+          upgradable: true,
+          installed_version: "1.2.0",
+          newest_version: "1.3.0",
+        },
+        "httpoison" => %{
+          upgradable: false,
+          installed_version: "0.10.0",
+          newest_version: "0.10.0",
+        },
+      }
+
+    reality = DepChecker.grab_deps
+
+    assert expectation == reality
+  end
 end
