fba9144e0fbab90ec0423435bdc34cfa8293eea7
Simone Vittori <hello@simonewebdesign.it>
2017-04-17 16:36:38 +0100

    Make sure it still works in dev

--- lib/depchecker/application.ex
+++ lib/depchecker/application.ex
@@ -6,10 +6,16 @@ defmodule DepChecker.Application do
   def start(_type, _args) do
     import Supervisor.Spec, warn: false
 
-    port = String.to_integer System.get_env "PORT"
+    cowboy_options =
+      if Mix.env == :prod do
+        [port: String.to_integer(System.get_env "PORT"),
+        ]
+      else
+        []
+      end
 
     children = [
-      Plug.Adapters.Cowboy.child_spec(:http, DepChecker.Router, [], [port: port])
+      Plug.Adapters.Cowboy.child_spec(:http, DepChecker.Router, [], cowboy_options),
     ]
 
     opts = [strategy: :one_for_one, name: DepChecker.Supervisor]
