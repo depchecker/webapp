97c4858c7e12ea679387c027b4f0a0732c260d0f
Simone Vittori <hello@simonewebdesign.it>
2017-04-28 17:32:31 +0100

    Remove comments

--- lib/depchecker/router.ex
+++ lib/depchecker/router.ex
@@ -1,33 +1,11 @@
 defmodule DepChecker.Router do
   use Plug.Router
 
-  # When the router is invoked, it will invoke the :match plug, represented by a
-  # local `match/2` function, and then call the :dispatch plug which will
-  # execute the matched code.
   plug :match
   plug Plug.Parsers, parsers:      [:json],
                      pass:         ["application/json"],
                      json_decoder: Poison
   plug :dispatch
-
-  # get "/" do
-  #   response = """
-  #   <!doctype html5>
-  #   <html>
-  #     <head>
-  #       <meta charset="utf-8">
-  #     </head>
-  #     <body>
-  #       <form method=post>
-  #         Project path: <input type=text name=project_path>
-  #         Dependency: <input type=text name=dependency>
-  #         <input type=submit>
-  #       </form>
-  #     </body>
-  #   </html>
-  #   """
-  #   send_resp(conn, 200, response)
-  # end
 
   post "/" do
     %{"package_name" => dep,
--- test/depchecker_test.exs
+++ test/depchecker_test.exs
@@ -9,11 +9,6 @@ defmodule DepCheckerTest do
           installed_version: "1.3.0-rc.1",
           newest_version: "1.3.0-rc.1",
         },
-        # "httpoison" => %{
-        #   upgradable: false,
-        #   installed_version: "0.10.0",
-        #   newest_version: "0.10.0",
-        # },
       }
 
     reality = DepChecker.perform("phoenix", "../../phoenix-demo-app/mix.lock")
